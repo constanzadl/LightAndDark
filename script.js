cf5a650ef9c39fdd1b7a327c7b46e1a9e31ac2a0
constanzadl <constanzadl94@gmail.com>
2020-09-28 17:36:09 -0600

    Light and dark mode

---
+++ script.js
@@ -0,0 +1,42 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+
+    image1.src = 'img/undraw_proud_coder_dark.svg';
+    image2.src = 'img/undraw_fixing_bugs_qkdj_dark.svg';
+    image3.src = 'img/undraw_conceptual_idea_dark.svg';
+}
+
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+
+    image1.src = 'img/undraw_proud_coder_light.svg';
+    image2.src = 'img/undraw_fixing_bugs_qkdj_light.svg';
+    image3.src = 'img/undraw_conceptual_idea_light.svg';
+}
+
+function switchTheme(event) {
+    if (event.target.checked){
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
+    }else{
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
