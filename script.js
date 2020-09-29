b194c52f2cbdb740b4cdd9125f2bc0e73bada010
constanzadl <constanzadl94@gmail.com>
2020-09-29 09:20:30 -0600

    revised code

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+function imageMode(color){
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_fixing_bugs_qkdj_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
 
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = 'Dark Mode';
     toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
 
-    image1.src = 'img/undraw_proud_coder_dark.svg';
-    image2.src = 'img/undraw_fixing_bugs_qkdj_dark.svg';
-    image3.src = 'img/undraw_conceptual_idea_dark.svg';
 }
 
 function lightMode() {
@@ -23,20 +26,31 @@ function lightMode() {
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = 'Light Mode';
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-
-    image1.src = 'img/undraw_proud_coder_light.svg';
-    image2.src = 'img/undraw_fixing_bugs_qkdj_light.svg';
-    image3.src = 'img/undraw_conceptual_idea_light.svg';
+    imageMode('light');
 }
 
 function switchTheme(event) {
     if (event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     }else{
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 toggleSwitch.addEventListener('change', switchTheme);
+
+//Check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme == 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
+console.log(currentTheme);
